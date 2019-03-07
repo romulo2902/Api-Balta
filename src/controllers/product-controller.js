47e55a8197a5857d2eae77fbbd706f66f1733cdf
romulo2902 <romulo2902@gmail.com>
2019-03-07 17:16:36 -0300

    04 - func.Get

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -2,22 +2,37 @@
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 
+exports.get = (req, res, next) => {
+    Product
+        .find({
+            active:true
+        },'title price slug'  )
+        .then(data => {
+            res.status(200).send(data);
+        }).catch(e => {
+            console.log(e)
+            res.status(400).send(e);
+        });
+}
+
+
 exports.post = (req, res, next) => {
     var product = new Product(req.body);
     product
         .save()
         .then(x => {
             res.status(201).send({
-                message:'Produto cadastrado com sucesso'
+                message: 'Produto cadastrado com sucesso'
             });
-    }).catch(e => { console.log(e)
-        res.status(400).send({
-            message:'Falha no  cadastrado do produto', 
-            data: e
+        }).catch(e => {
+            console.log(e)
+            res.status(400).send({
+                message: 'Falha no  cadastrado do produto',
+                data: e
+            });
         });
-    });
-            
-};
+
+}
 
 exports.put = ('/', (req, any, next) => {
     const id = req.params.id;
