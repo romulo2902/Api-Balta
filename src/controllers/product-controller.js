ebe9931490d94eb2a871f5565677e9cf38da7e84
romulo2902 <romulo2902@gmail.com>
2019-03-09 16:36:51 -0300

    08 - Validação  funcionando

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,6 +1,7 @@
 'use strict'
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
+const ValidationContract = require('../validators/fluent-validator');
 
 exports.get = (req, res, next) => {
     Product
@@ -55,6 +56,19 @@ exports.getByTag = (req, res, next) => {
 }
 
 exports.post = (req, res, next) => {
+    let contract = new ValidationContract();
+    contract.hasMinLen(req.body.title, 3, 'O título deve conter pelo menos 3 caracteres');
+    contract.hasMinLen(req.body.slug, 3, 'O Slug deve conter pelo menos 3 caracteres');
+    contract.hasMinLen(req.body.description, 3, 'A Descrição deve conter pelo menos 3 caracteres');
+
+    //Se os dados forem inválidos
+
+    if (!contract.isValid()) {
+        res.status(400).send(contract.errors()).end();
+        return;
+
+    }
+
     var product = new Product(req.body);
     product
         .save()
