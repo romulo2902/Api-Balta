deb54cf01b59f511969e6aebde1e7bad2b792f7f
romulo2902 <romulo2902@gmail.com>
2019-04-01 17:10:02 -0300

    10 - arrumei

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -4,50 +4,67 @@ const Product = mongoose.model('Product');
 const ValidationContract = require('../validators/fluent-validator');
 const repository = require('../repositories/product-repository');
 
-exports.get = (req, res, next) => {
-    repository
-        .get()
-        .then(data => {
-            res.status(200).send(data);
-        }).catch(e => {
-            console.log(e)
-            res.status(400).send(e);
+exports.get = async (req, res, next) => {
+
+    try {
+        var data = await repository.get();
+        res.status(200).send(data);
+
+    } catch (e) {
+        res.status(500).send({
+            message: 'Falha ao processar sua requisição'
+
         });
+
+    }
+
 }
 
-exports.getBySlug = (req, res, next) => {
-    repository
-        .getBySlug(req.params.slug)
-        .then(data => {
+
+exports.getBySlug = async(req, res, next) => {
+    try{
+        var data = await repository.getBySlug(req.params.slug);
             res.status(200).send(data);
-        }).catch(e => {
-            console.log(e)
-            res.status(400).send(e);
+       
+    }catch (e) {
+        res.status(500).send({
+            message: 'Falha ao processar sua requisição'
+
         });
+
+    }
+
 }
 
-exports.getById = (req, res, next) => {
-    repository
-        .getById(req.params.id)
-        .then(data => {
-            res.status(200).send(data);
-        }).catch(e => {
-            console.log(e)
-            res.status(400).send(e);
+
+exports.getById = async(req, res, next) => {
+    try {
+
+        var data = await repository.getById(req.params.id);
+        res.status(200).send(data);
+
+    }catch (e) {
+        res.status(500).send({
+            message: 'Falha ao processar sua requisição'
+
         });
+    }
 }
 
-exports.getByTag = (req, res, next) => {
-    repository
-        .getByTag(req.params.tag)
-        .then(data => {
-            res.status(200).send(data);
-        }).catch(e => {
-            console.log(e)
-            res.status(400).send(e);
-        });
-}
 
+exports.getByTag = async(req, res, next) => {
+  try{
+
+    const data = await repository.getByTag(req.params.tag);
+    res.status(200).send(data);
+
+  }catch (e) {
+    res.status(500).send({
+        message: 'Falha ao processar sua requisição'
+
+    });
+}
+}
 exports.post = (req, res, next) => {
     let contract = new ValidationContract();
     contract.hasMinLen(req.body.title, 3, 'O título deve conter pelo menos 3 caracteres');
@@ -95,7 +112,7 @@ exports.put = (req, res, next) => {
 };
 
 exports.delete = (req, res, next) => {
-        repository
+    repository
         .delete(req.body.id)
         .then(x => {
             res.status(200).send({
